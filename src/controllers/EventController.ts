be0c471f83610983c21f9ce7b4cae79e71bdf9cb
easymoneyspike <euryanspike.pro@gmail.com>
2024-07-24 21:48:46 -0300

    slug nas rotas

--- src/controllers/EventController.ts
+++ src/controllers/EventController.ts
@@ -1,14 +1,18 @@
 import { PrismaClient } from "@prisma/client";
 import { Request, Response } from "express";
 
+import slugify from "slugify";
+
 const prisma = new PrismaClient();
 
 export const createEvent = async (req: Request, res: Response) => {
-    const { name, details, slug, maximumAttendees } = req.body;
+    const { name, details, maximumAttendees } = req.body;
 
-    if(!name || !details || !slug || !maximumAttendees){
+    if(!name || !details || !maximumAttendees){
         return res.status(400).json({ message: 'todos os campos são obrigatório' });
     }
+
+    const slug = slugify(name, {lower: true}); //criando slug do evento
 
     try{
         const event = await prisma.event.create({
@@ -30,7 +34,7 @@ export const createEvent = async (req: Request, res: Response) => {
     }
 }
 
-export const getEvents = async  (req: Request, res: Response) => {
+export const getEvents = async (req: Request, res: Response) => {
 
     try{
         const events = await prisma.event.findMany();
@@ -38,5 +42,26 @@ export const getEvents = async  (req: Request, res: Response) => {
         return res.status(400).json({ message: events }); 
     }catch(err){
         console.log(err);
+    }
+}
+
+export const getEventBySlug = async  (req: Request, res: Response) => {
+    const { slug } = req.params;
+
+    try{
+        const eventBySlug = await prisma.event.findUnique({
+            where: { slug }
+        })
+
+        //se achar o evento pelo slug, retorna o evento
+        if(eventBySlug){
+            const { slug, ...eventWithoutSlug } = eventBySlug; //tirar o slug da data retornada pro client
+            return res.status(200).json({ message: eventWithoutSlug })
+        }
+
+        return res.status(404).json({ message: 'evento não encontrado.' })
+    }catch(err){
+        console.log(err);
+        return res.status(500).json({ message: 'erro no servidor.' })
     }
 }
--- src/router/EventRouter.ts
+++ src/router/EventRouter.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
-import { createEvent, getEvents } from "../controllers/EventController";
+import { createEvent, getEventBySlug, getEvents } from "../controllers/EventController";
 
 const router = Router();
 
 router
     .post('/event', createEvent)
     .get('/event', getEvents)
+    .get('/event/:slug', getEventBySlug)
+
 
 export default router;
